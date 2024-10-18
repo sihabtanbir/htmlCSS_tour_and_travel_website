d718b18789241c6194dc8b3ce25bc8120ee7a36e
Tanbir Ahmmed Sihab <sihab.cse@gmail.com>
2024-10-18 19:06:44 +0600

    header

---
+++ custom.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $(' .navbar').toggleClass('nav-toggle');
+    })
+
+
+    $(window).on('load scroll',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClassClass('nav-toggle');
+
+        if($(window).scrollTop()>30){
+            $('header').addClass('header-active')
+        }else{
+            $('header').removeClass('header-active')
+        }
+    })
+
+})
